f39fe1c88cce70121c8646f5dc9cfd1c9c55bc40
MikeYoulend <mikeyoulend@gmail.com>
2024-02-22 18:25:18 +0100

    Code Lecture

--- Assets/Obstacle/BonusSpawnPoint.cs
+++ Assets/Obstacle/BonusSpawnPoint.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class BonusSpawnPoint : MonoBehaviour
 {
-    public GameObject bonusPrefab;
-    public GameObject parentGameObject;
-    public bool Left = true;
+    [SerializeField] GameObject bonusPrefab;
+    [SerializeField] GameObject parentGameObject;
+    [SerializeField] bool Left = true;
     
     float Xposition;
     float Yposition;
@@ -18,29 +18,27 @@ public class BonusSpawnPoint : MonoBehaviour
        parentGameObject = GameObject.FindWithTag("SpawnBonus");
        Invoke("SpawnBonus", 1.5f);
     }
-
-    // Update is called once per frame
-    void Update()
+    void SpawnBonus()
     {
-        
+        WhereIsPositioned();
+
+        Vector3 spawnPosition = new Vector3(Xposition, Yposition, RandomZ);
+
+        GameObject newBonus = Instantiate(bonusPrefab, spawnPosition, Quaternion.identity);
+        newBonus.transform.parent = parentGameObject.transform;
     }
 
-    void SpawnBonus()
-    {   
-        if(Left == true)
+    private void WhereIsPositioned()
+    {
+        if (Left == true)
         {
-        Xposition = -20f;   
+            Xposition = -20f;
         }
         else
         {
-        Xposition = 20f; 
+            Xposition = 20f;
         }
         Yposition = 0.60f;
-        RandomZ = Random.Range(7, -7);    
-
-        Vector3 spawnPosition = new Vector3(Xposition, Yposition, RandomZ);
-        
-        GameObject newBonus = Instantiate(bonusPrefab, spawnPosition, Quaternion.identity);
-        newBonus.transform.parent = parentGameObject.transform;
+        RandomZ = Random.Range(7, -7);
     }
 }
